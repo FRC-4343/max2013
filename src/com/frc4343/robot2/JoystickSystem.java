344eeb5a83d15d6b938eb52cbcef4d1caf099c9b
Nikita Pekin <nikitapekin@gmail.com>
2013-03-21 20:47:50 -0400

    Added JoystickSystem to handle joystick code

---
+++ src/com/frc4343/robot2/JoystickSystem.java
@@ -0,0 +1,23 @@
+package com.frc4343.robot2;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class JoystickSystem {
+
+    final static byte JOYSTICK_COUNT = 2;
+    Joystick[] joysticks = new Joystick[JOYSTICK_COUNT];
+
+    public JoystickSystem() {
+        for (int i = 0; i < JOYSTICK_COUNT; i++) {
+            joysticks[i] = new Joystick(i);
+        }
+    }
+
+    public Joystick getJoystick(int joystickNumber) {
+        if (joysticks.length < joystickNumber && joysticks[joystickNumber] != null) {
+            return joysticks[joystickNumber - 1];
+        } else {
+            return null;
+        }
+    }
+}
